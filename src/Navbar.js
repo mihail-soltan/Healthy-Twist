6db5ebad0dcee5efe4ab588fcfb2f40c59cd16ec
sherinececil <sherinececil@gmail.com>
2021-07-24 18:07:42 +0200

    Added Sidebar feature in Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,41 @@
 import Nav from 'react-bootstrap/Nav';
+import React, {useState} from 'react'
 import { BoxArrowInRight, List } from 'react-bootstrap-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import "./Navbar.css";
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import Button from 'react-bootstrap/Button'
 
 export default function Navbar() {
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
     return (
             <Nav className="navbar">
                 <Nav.Item className="hamburgerMenu">
                     <Nav.Link className="nav-link">
-                        <List className="m-2" size={30}/>
+                        <List className="m-2" size={30} onClick={handleShow} />
+                        
+                        <Offcanvas show={show} onHide={handleClose}
+                        bg ="dark">
+                            
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Welcome</Offcanvas.Title>
+         
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+            <div className="d-grid gap-2">
+        <Button variant="secondary" size="sm">Most Popular</Button> 
+        
+        <Button variant="secondary" size="sm">Holiday Special</Button> 
+        <br />
+        </div>
+        </Offcanvas.Body>
+        
+      </Offcanvas>
+     
                     </Nav.Link>
                 </Nav.Item>
               
