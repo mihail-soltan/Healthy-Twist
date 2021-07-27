47d430c83922422ba3341180e866a3e951d223d9
mihail-soltan <78448500+mihail-soltan@users.noreply.github.com>
2021-07-27 17:27:51 +0200

    Revert "added chef component"

--- src/RecipeCarousel.js
+++ src/RecipeCarousel.js
@@ -2,7 +2,7 @@ import Carousel from "react-bootstrap/Carousel"
 import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
 import { useState } from "react";
-export default function RecipeCarousel({recipes}) {
+export default function RecipeCarousel() {
     const [index, setIndex] = useState(0);
 
     const handleSelect = (selectedIndex, e) => {
@@ -11,29 +11,10 @@ export default function RecipeCarousel({recipes}) {
 
     return (
         <Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
+            
             <Carousel.Item>
                 <div className="recipes">
-            {recipes.map((item) =>
-                <Card style={{ width: '20em' }}>
-                    <Card.Img variant="top" src="https://www.thewholesomedish.com/wp-content/uploads/2019/06/The-Best-Classic-Tacos-550.jpg" />
-                    <Card.Body>
-                        <Card.Title>{item.fields.title}</Card.Title>
-                        <Card.Text>{item.fields.userStroyAboutRecepie}
-                        </Card.Text>
-                        <Button variant="success">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                    )}
-                {/* <Card style={{ width: '18rem' }}> */}
-                    {/* <Card.Img variant="top" src="https://www.thewholesomedish.com/wp-content/uploads/2019/06/The-Best-Classic-Tacos-550.jpg" /> */}
-                    {/* <Card.Body> */}
-                        {/* <Card.Title>{item.fields.title}</Card.Title> */}
-                        {/* <Card.Text>{item.fields.userStroyAboutRecepie} */}
-                        {/* </Card.Text> */}
-                        {/* <Button variant="success">Go somewhere</Button> */}
-                    {/* </Card.Body> */}
-                {/* </Card> */}
-                {/* <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="https://www.thewholesomedish.com/wp-content/uploads/2019/06/The-Best-Classic-Tacos-550.jpg" />
                     <Card.Body>
                         <Card.Title>Card Title</Card.Title>
@@ -54,11 +35,22 @@ export default function RecipeCarousel({recipes}) {
                         </Card.Text>
                         <Button variant="success">Go somewhere</Button>
                     </Card.Body>
-                </Card> */}
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://www.thewholesomedish.com/wp-content/uploads/2019/06/The-Best-Classic-Tacos-550.jpg" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="success">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
 
                 </div>
             </Carousel.Item>
-            {/* <Carousel.Item>
+            <Carousel.Item>
             <div className="recipes">
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="https://www.thewholesomedish.com/wp-content/uploads/2019/06/The-Best-Classic-Tacos-550.jpg" />
@@ -135,7 +127,7 @@ export default function RecipeCarousel({recipes}) {
                 </Card>
 
                 </div>
-            </Carousel.Item> */}
+            </Carousel.Item>
         </Carousel>
     );
 }
